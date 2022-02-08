69b96b1da1e88b8f30c65ee841fff4a484d21f72
Francisco Power <33499683+franciscopower@users.noreply.github.com>
2022-02-08 18:33:56 +0100

    check setup directory

--- setup.sh
+++ setup.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
-echo "Starting setup script..."
+if [ "$HOME/.dotfiles" != "$PWD" ]
+then 
+	echo "Setup script cannot be run." 
+	echo "To run the scrip, mas sure the current directory is $HOME/.dotfiles"
+	echo "To do so, place the .dotfiles directory in the $HOME directory and run the setup script from inside the .dotfiles directory."
+	exit 1
+fi
 
+echo "Starting setup script..."
+echo -e "\n -----------------------------------------------"
+
+echo -e "\n Updating..."
 # update and upgrade
 apt update
-apt upgrade -y
+echo -e "\n -----------------------------------------------"
 
+echo -e "\n Upgrading..."
+apt upgrade -y
+echo -e "\n -----------------------------------------------"
+
+echo -e "\n Installing Dependecies..."
 # install dependencies
 yes | apt install vim
 yes | apt install htop 
 yes | apt install neofetch  
 yes | apt install zsh 
+echo -e "\n -----------------------------------------------"
 
-echo -e "\n\n Finnished installing dependencies"
-echo -e "\n\n Creating symbolic links"
-
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
-ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
-ln -s $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
-
-
-
-
-
-
-
-
+echo -e "\n Creating symbolic links\n"
+ln -s $PWD/.zshrc $HOME/.zshrc
+ln -s $PWD/.vimrc $HOME/.vimrc
+ln -s $PWD/.gitconfig $HOME/.gitconfig
