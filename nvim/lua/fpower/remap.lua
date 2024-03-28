77f927731b9c6eac1458aae69a67cfc7f6d29267
Francisco Power <33499683+franciscopower@users.noreply.github.com>
2024-03-28 16:04:14 +0000

    Update remap.lua

--- nvim/lua/fpower/remap.lua
+++ nvim/lua/fpower/remap.lua
@@ -7,6 +7,25 @@ vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 
+-- swtich between tabs
+vim.keymap.set("n", "<leader>1", "1gt")
+vim.keymap.set("n", "<leader>2", "2gt")
+vim.keymap.set("n", "<leader>3", "3gt")
+vim.keymap.set("n", "<leader>4", "4gt")
+vim.keymap.set("n", "<leader>5", "5gt")
+vim.keymap.set("n", "<leader>6", "6gt")
+vim.keymap.set("n", "<leader>7", "7gt")
+vim.keymap.set("n", "<leader>8", "8gt")
+vim.keymap.set("n", "<leader>9", "9gt")
+
+vim.keymap.set("n", "<leader>h", function()
+    vim.cmd("tabprevious")
+end)
+
+vim.keymap.set("n", "<leader>l", function()
+    vim.cmd("tabnext")
+end)
+
 -- move highlited lines together
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -21,24 +40,12 @@ vim.keymap.set("n", "N", "Nzzzv")
 -- do not copy when pasting over something
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- next greatest remap ever : asbjornHaland
+-- yank into system clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
--- yank into system clipboard
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
-
--- other escape
-vim.keymap.set("i", "çç", "<Esc>")
-
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
