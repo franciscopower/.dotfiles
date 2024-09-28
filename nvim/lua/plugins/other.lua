b6b3c0d5f2bd82d4ba7215aeb6309f61d7c00ba0
Francisco Power <33499683+franciscopower@users.noreply.github.com>
2024-09-28 12:58:42 +0100

    remove minimap plugin, add scrollbar plugin and quick switcher

---
+++ nvim/lua/plugins/other.lua
@@ -0,0 +1,23 @@
+return {
+  'rgroli/other.nvim',
+  config = function ()
+    require("other-nvim").setup({
+      mappings = {
+        "angular",
+        "python",
+        "rust",
+      },
+    })
+
+    vim.api.nvim_set_keymap("n", "<leader>o", "<cmd>:Other<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<leader>ott", "<cmd>:OtherTabNew<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<leader>ov", "<cmd>:OtherVSplit<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<leader>oc", "<cmd>:OtherClear<CR>", { noremap = true, silent = true })
+
+    -- Context specific bindings
+    vim.api.nvim_set_keymap("n", "<leader>ot", "<cmd>:Other test<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<leader>os", "<cmd>:Other scss<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<leader>oh", "<cmd>:Other html<CR>", { noremap = true, silent = true })
+
+  end
+}
