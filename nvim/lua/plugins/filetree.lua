8278c24b3f86337812a653fce7f2d0d466943cc6
EMEA\POF1AV <francisco.power@pt.bosch.com>
2024-09-30 08:36:59 +0100

    improve ctrl+b shortcut to toggle file tree

--- nvim/lua/plugins/filetree.lua
+++ nvim/lua/plugins/filetree.lua
@@ -8,6 +8,8 @@ return {
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
-    vim.keymap.set('n', '<C-b>', ':Neotree filesystem toggle left<CR>', {})
+    vim.keymap.set("n", "<C-b>", function()
+      vim.cmd("Neotree filesystem toggle left")
+    end)
   end
 }
