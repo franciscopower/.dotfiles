d591d660c5662deb00d556a09c4d35325e81ec90
EMEA\POF1AV <francisco.power@pt.bosch.com>
2024-10-01 10:23:35 +0100

    add gitsigns keymaps

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -2,20 +2,20 @@ return {
   {
     "tpope/vim-fugitive",
     config = function ()
-      vim.keymap.set("n", "<leader>gB",
-        function()
-          vim.cmd("Git blame")
-        end,
-        { silent = true, noremap = true }
-      )
-    end
+      vim.keymap.set("n", "<leader>gB", function() vim.cmd("Git blame") end, { silent = true, noremap = true }) end
   },
   {
     "lewis6991/gitsigns.nvim",
     config = function()
-      require('gitsigns').setup()
+      local gs = require('gitsigns')
+      gs.setup()
 
-      vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', {})
+      vim.keymap.set('n', '<leader>hp', gs.preview_hunk, {})
+      vim.keymap.set('n', '<leader>hs', gs.stage_hunk, {})
+      vim.keymap.set('n', '<leader>hu', gs.undo_stage_hunk, {})
+      vim.keymap.set('n', '<leader>hr', gs.reset_hunk, {})
+      vim.keymap.set('n', '<leader>hb', gs.toggle_current_line_blame, {})
+      vim.keymap.set('n', '<leader>hd', gs.diffthis, {})
     end
   },
   {
@@ -24,9 +24,7 @@ return {
   {
     "NeogitOrg/neogit",
     dependencies = {
-      "nvim-lua/plenary.nvim",  -- required
-      "sindrets/diffview.nvim", -- optional - Diff integration
-
+      "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope.nvim",
     },
     config = function()
@@ -34,30 +32,10 @@ return {
       neogit.setup()
 
       vim.keymap.set("n", "<leader>gs", neogit.open, { silent = true, noremap = true })
-      vim.keymap.set("n", "<leader>gc",
-        function()
-          vim.cmd("Neogit commit")
-        end,
-        { silent = true, noremap = true }
-      )
-      vim.keymap.set("n", "<leader>gp",
-        function()
-          vim.cmd("Neogit pull")
-        end,
-        { silent = true, noremap = true }
-      )
-      vim.keymap.set("n", "<leader>gP",
-        function()
-          vim.cmd("Neogit push")
-        end,
-        { silent = true, noremap = true }
-      )
-      vim.keymap.set("n", "<leader>gb",
-        function()
-          vim.cmd("Telescope git_branches")
-        end,
-        { silent = true, noremap = true }
-      )
+      vim.keymap.set("n", "<leader>gc", function() vim.cmd("Neogit commit") end, { silent = true, noremap = true })
+      vim.keymap.set("n", "<leader>gp", function() vim.cmd("Neogit pull") end, { silent = true, noremap = true })
+      vim.keymap.set("n", "<leader>gP", function() vim.cmd("Neogit push") end, { silent = true, noremap = true })
+      vim.keymap.set("n", "<leader>gb", function() vim.cmd("Telescope git_branches") end, { silent = true, noremap = true })
     end
   }
 }
