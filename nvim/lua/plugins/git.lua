8ee8aefa21c32df3de1435345bdf9bdce7257685
EMEA\POF1AV <francisco.power@pt.bosch.com>
2024-10-01 09:58:32 +0100

    add and configure neogit

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,10 +1,18 @@
 return {
   {
-    "tpope/vim-fugitive"
+    "tpope/vim-fugitive",
+    config = function ()
+      vim.keymap.set("n", "<leader>gB",
+        function()
+          vim.cmd("Git blame")
+        end,
+        { silent = true, noremap = true }
+      )
+    end
   },
   {
     "lewis6991/gitsigns.nvim",
-    config = function ()
+    config = function()
       require('gitsigns').setup()
 
       vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', {})
@@ -13,4 +21,43 @@ return {
   {
     "sindrets/diffview.nvim",
   },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",  -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      local neogit = require('neogit')
+      neogit.setup()
+
+      vim.keymap.set("n", "<leader>gs", neogit.open, { silent = true, noremap = true })
+      vim.keymap.set("n", "<leader>gc",
+        function()
+          vim.cmd("Neogit commit")
+        end,
+        { silent = true, noremap = true }
+      )
+      vim.keymap.set("n", "<leader>gp",
+        function()
+          vim.cmd("Neogit pull")
+        end,
+        { silent = true, noremap = true }
+      )
+      vim.keymap.set("n", "<leader>gP",
+        function()
+          vim.cmd("Neogit push")
+        end,
+        { silent = true, noremap = true }
+      )
+      vim.keymap.set("n", "<leader>gb",
+        function()
+          vim.cmd("Telescope git_branches")
+        end,
+        { silent = true, noremap = true }
+      )
+    end
+  }
 }
