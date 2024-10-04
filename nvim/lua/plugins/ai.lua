a6a425a84824920d2261342ac50d64ad6aea147d
EMEA\POF1AV <francisco.power@pt.bosch.com>
2024-10-04 18:09:14 +0100

    edit copilot shortcuts

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,5 +1,19 @@
 return {
-  { "github/copilot.vim" }
+  {
+    "github/copilot.vim",
+    config = function ()
+      vim.keymap.set('i', '<C-k>', 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false,
+        desc = "Coplilot accept"
+      })
+      vim.g.copilot_no_tab_map = true
+      -- vim.keymap.set('i', '<leader>cj', '<Plug>(copilot-accept-word)')
+      -- vim.keymap.set('i', '<leader>ch', '<Plug>(copilot-previous)')
+      -- vim.keymap.set('i', '<leader>cl', '<Plug>(copilot-next)')
+
+    end
+  }
   -- {
   --   "Exafunction/codeium.vim",
   --   config = function()
