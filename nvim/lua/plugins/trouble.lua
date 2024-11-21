f08f3355c5d72ecc23f9817c1c8b7a4c8c3c02d6
EMEA\POF1AV <francisco.power@pt.bosch.com>
2024-11-21 12:39:24 +0000

    change keymaps to be inline with gs (show diagnostics)

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -4,12 +4,12 @@ return {
   cmd = "Trouble",
   keys = {
     {
-      "<leader>vd",
+      "<leader>vl",
       "<cmd>Trouble diagnostics toggle<cr>",
       desc = "Diagnostics (Trouble)",
     },
     {
-      "<leader>vl",
+      "<leader>vd",
       "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
       desc = "LSP Definitions / references / ... (Trouble)",
     },
