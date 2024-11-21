c3dbc091f852b5ad9804e12a9d0b140f90fced08
EMEA\POF1AV <francisco.power@pt.bosch.com>
2024-11-21 09:55:21 +0000

    remove unnecessary plugin

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -7,13 +7,4 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
   },
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && npm install",
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
-  },
 }
