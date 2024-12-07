c896e2fa83737c10a44a2f4bdadd83cc69ffd09d
Francisco Power <33499683+franciscopower@users.noreply.github.com>
2024-12-07 17:08:28 +0000

    remove lsp-zero dependency

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,106 +1,113 @@
 return {
-
-  "VonHeikemen/lsp-zero.nvim",
-  branch = "v2.x",
-  dependencies = {
-    -- LSP
-    { "neovim/nvim-lspconfig" },
-    { "williamboman/mason.nvim" },
-    { "williamboman/mason-lspconfig.nvim" },
-    -- Autocompletion
-    { "hrsh7th/nvim-cmp" },
-    { "hrsh7th/cmp-nvim-lsp" },
-    { "L3MON4D3/LuaSnip" },
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		-- Autocompletion
+		{ "williamboman/mason-lspconfig.nvim" },
+		{ "hrsh7th/nvim-cmp" },
+		{ "hrsh7th/cmp-nvim-lsp" },
+		{ "L3MON4D3/LuaSnip" },
     { "saadparwaiz1/cmp_luasnip" },
     { "rafamadriz/friendly-snippets" },
-  },
-  config = function()
-    -- require packages
-    local lsp = require("lsp-zero")
-    local cmp = require("cmp")
-    require("luasnip.loaders.from_vscode").lazy_load()
-    -- Fix Undefined global 'vim'
-    lsp.nvim_workspace()
+	},
+	config = function()
+		-- for mason to play nice with LSP config
+		require("mason-lspconfig").setup()
+		require("luasnip.loaders.from_vscode").lazy_load()
 
-    -------------------
-    --- lsp configs ---
-    -------------------
-    lsp.preset("recommended")
+		local lspconfig = require("lspconfig")
 
-    lsp.set_preferences({
-      suggest_lsp_servers = false,
-      sign_icons = {
-        error = " ",
-        warn = " ",
-        hint = "󰌵",
-        info = " ",
-      },
-    })
+		-- Add cmp_nvim_lsp capabilities settings to lspconfig
+		-- This should be executed before you configure any language server
+		local lspconfig_defaults = require("lspconfig").util.default_config
+		lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+			"force",
+			lspconfig_defaults.capabilities,
+			require("cmp_nvim_lsp").default_capabilities()
+		)
 
-    -------------------
-    -- cmp configs ---
-    -------------------
-    local cmp_select = { behavior = cmp.SelectBehavior.Select }
-    lsp.setup_nvim_cmp({
-      mapping = lsp.defaults.cmp_mappings({
-        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<S-Tab>"] = nil,
-      }),
-      snippet = {
-        expand = function(args)
-          -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-          require("luasnip").lsp_expand(args.body)
-        end,
-      },
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "luasnip" }, -- For luasnip users.
-      }, {
-        { name = "buffer" },
-      }),
-    })
 
-    ------------------
-    -- keybindings ---
-    ------------------
-    lsp.on_attach(function(_, bufnr)
-      vim.keymap.set("n", "gd", function()
-        vim.lsp.buf.definition()
-      end, { buffer = bufnr, remap = false, desc = "Goto definition" })
-      vim.keymap.set("n", "gD", function()
-        vim.lsp.buf.declaration()
-      end, { buffer = bufnr, remap = false, desc = "Goto declaration" })
-      vim.keymap.set("n", "K", function()
-        vim.lsp.buf.hover()
-      end, { buffer = bufnr, remap = false, desc = "Show hover definition" })
-      vim.keymap.set("n", "ge", function()
-        vim.diagnostic.goto_next()
-      end, { buffer = bufnr, remap = false, desc = "Goto next error" })
-      vim.keymap.set("n", "gE", function()
-        vim.diagnostic.goto_prev()
-      end, { buffer = bufnr, remap = false, desc = "Goto prev error" })
-      vim.keymap.set("n", "<leader>ga", function()
-        vim.lsp.buf.code_action()
-      end, {buffer = bufnr, remap = false, desc = "Code actions"})
-      vim.keymap.set("n", "<leader>rn", function()
-        vim.lsp.buf.rename()
-      end, { buffer = bufnr, remap = false, desc = "Rename" })
-    end)
+		--------------------
+		-- setup all LSPs --
+		--------------------
+		lspconfig.lua_ls.setup({})
+		lspconfig.eslint.setup({})
+		lspconfig.ts_ls.setup({})
+		lspconfig.angularls.setup({})
+		lspconfig.pyright.setup({})
+		lspconfig.html.setup({})
+		lspconfig.cssls.setup({})
+		lspconfig.clangd.setup({})
 
-    -- run setup function
-    lsp.setup()
-    vim.diagnostic.config({
-      underline = true,
-      virtual_text = {
-        severity = { min = vim.diagnostic.severity.WARN },
-      },
-    })
-  end,
+		----------------------
+		-- setup completion --
+		----------------------
+		local cmp = require("cmp")
+
+		cmp.setup({
+			snippet = {
+				-- REQUIRED - you must specify a snippet engine
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+					-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+				end,
+			},
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" }, -- For luasnip users.
+			}, {
+				{ name = "buffer" },
+			}),
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<TAB>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+			}),
+		})
+
+		-----------------------
+		-- setup keybindings --
+		-----------------------
+		vim.api.nvim_create_autocmd("LspAttach", {
+			desc = "LSP actions",
+			callback = function(event)
+				vim.keymap.set("n", "gd", function()
+					vim.lsp.buf.definition()
+				end, { buffer = bufnr, remap = false, desc = "Goto definition" })
+				vim.keymap.set("n", "gD", function()
+					vim.lsp.buf.declaration()
+				end, { buffer = bufnr, remap = false, desc = "Goto declaration" })
+				vim.keymap.set("n", "K", function()
+					vim.lsp.buf.hover()
+				end, { buffer = bufnr, remap = false, desc = "Show hover definition" })
+				vim.keymap.set("n", "ge", function()
+					vim.diagnostic.goto_next()
+				end, { buffer = bufnr, remap = false, desc = "Goto next error" })
+				vim.keymap.set("n", "gE", function()
+					vim.diagnostic.goto_prev()
+				end, { buffer = bufnr, remap = false, desc = "Goto prev error" })
+				vim.keymap.set("n", "<leader>ga", function()
+					vim.lsp.buf.code_action()
+				end, { buffer = bufnr, remap = false, desc = "Code actions" })
+				vim.keymap.set("n", "<leader>rn", function()
+					vim.lsp.buf.rename()
+				end, { buffer = bufnr, remap = false, desc = "Rename" })
+			end,
+		})
+	end,
+
+	--------------------------
+	-- other configurations --
+	--------------------------
+	vim.diagnostic.config({
+		underline = true,
+		virtual_text = {
+			severity = { min = vim.diagnostic.severity.WARN },
+		},
+	}),
 }
