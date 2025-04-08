868ccbf38f8a1825ef3293bbee5b69a225c706bf
EMEA\POF1AV <francisco.power@pt.bosch.com>
2025-04-08 18:51:35 +0100

    replace copilot.nvim with avante

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,58 +1,62 @@
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    opts = {
-      panel = {
-        enabled = false, -- default true
-      },
-      suggestion = {
-        enabled = true,
-        auto_trigger = true, -- default false
-        hide_during_completion = true,
-        debounce = 75,
-        keymap = {
-          accept = "<C-k>", -- default "<M-l>",
-          accept_word = false,
-          accept_line = false,
-          next = "<M-]>",
-          prev = "<M-[>",
-          dismiss = "<C-]>",
-        },
-      },
-      filetypes = {
-        yaml = false,
-        markdown = false,
-        help = false,
-        gitcommit = true, -- default false
-        gitrebase = false,
-        hgcommit = false,
-        svn = false,
-        cvs = false,
-        ["."] = false,
-      },
-    },
-  },
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-    },
-    branch = "main",
-    config = function()
-      require("CopilotChat").setup()
-
-      vim.keymap.set("n", "<leader>ac", ":CopilotChat")                -- Open the chat window
-      vim.keymap.set("n", "<leader>ai", ":CopilotChatToggle<CR>")      -- Toggle the chat window
-      vim.keymap.set("v", "<leader>ae", ":CopilotChatExplain<CR>")     -- Write an explanation for the active selection as paragraphs of text
-      vim.keymap.set("v", "<leader>ar", ":CopilotChatReview<CR>")      -- Review the selected code
-      vim.keymap.set("v", "<leader>af", ":CopilotChatFix<CR>")         -- There is a problem in this code. Rewrite the code to show it with the bug fixed
-      vim.keymap.set("v", "<leader>ao", ":CopilotChatOptimize<CR>")    -- Optimize the selected code to improve performance and readability
-      vim.keymap.set("v", "<leader>ad", ":CopilotChatDocs<CR>")        -- Please a-dd documentation comment for the selection
-      vim.keymap.set({ "v", "n" }, "<leader>at", ":CopilotChatTests<CR>") -- Please generate tests for my code
-      -- vim.keymap.set("n", "<leader>a", ":CopilotChatFixDiagnostic<CR>") -- Please assist with the following diagnostic issue in file
-      vim.keymap.set("n", "<leader>ag", ":CopilotChatCommitStaged<CR>") -- Write commit message for the change with commitizen convention
-    end,
-  },
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		version = false, -- Never set this value to "*"! Never!
+		-- build = "make", -- for linux
+		build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false", -- for windows
+		opts = {
+			provider = "copilot",
+			auto_suggestions_provider = "copilot",
+			behaviour = {
+				auto_suggestions = true,
+				support_paste_from_clipboard = true,
+        enable_token_counting = false,
+			},
+			hints = { enabled = false },
+			mappings = {
+				suggestion = {
+					accept = "<C-k>",
+				},
+			},
+		},
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"echasnovski/mini.pick", -- for file_selector provider mini.pick
+			"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+			"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+			"ibhagwan/fzf-lua", -- for file_selector provider fzf
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			"zbirenbaum/copilot.lua", -- for providers='copilot'
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = true,
+					},
+				},
+			},
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
+	},
 }
