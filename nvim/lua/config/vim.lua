dd85c77b51bd2166901c2d8f933c5f30e6ad5079
EMEA\POF1AV <francisco.power@pt.bosch.com>
2025-04-29 10:03:00 +0100

    add highlight on yank to nvim config

--- nvim/lua/config/vim.lua
+++ nvim/lua/config/vim.lua
@@ -124,3 +124,13 @@ vim.keymap.set("n", "L", "$")
 -- replace all occurences of the selected block
 vim.keymap.set("v", "<leader>s", "\"hy:%s/<C-r>h//g<left><left>", {desc = "substitute selected block"})
 vim.keymap.set("v", "<leader>/", "\"hy/<C-r>h", {desc = "search selected block"})
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight_yank', {}),
+  desc = 'Hightlight selection on yank',
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 200 }
+  end,
+})
