761b3af2de1813050c3a0c6f24b23f01bf24becf
EMEA\POF1AV <francisco.power@pt.bosch.com>
2025-11-05 16:02:02 +0000

    fix formatting with conform

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -1,19 +1,29 @@
 return {
   'stevearc/conform.nvim',
-  opts = {},
-  formatters_by_ft = {
-    lua = { 'stylua' },
-    javascript = { 'prettier' },
-    typescript = { 'prettier' },
-    python = { 'black', 'isort' },
-    xml = { 'xmlformatter' },
-    go = { 'gofumpt', 'goimports', 'golines' },
-    html = { 'prettier' },
-    css = { 'prettier' },
-    htmlangular = { 'prettier' },
-    json = { 'prettier' },
-  },
   config = function()
-    vim.keymap.set("n", "<leader>nn", vim.lsp.buf.format, { desc = "Format buffer" })
+    require('conform').setup({
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
+        python = { 'black', 'isort' },
+        xml = { 'xmlformatter' },
+        go = { 'gofumpt', 'goimports', 'golines' },
+        html = { 'prettier' },
+        css = { 'prettier' },
+        htmlangular = { 'prettier' },
+        json = { 'prettier' },
+      },
+      formatters = {
+        prettier = {
+          prepend_args = {
+            "--config-precedence",
+            "prefer-file",
+          }
+        },
+      },
+
+    })
+    vim.keymap.set("n", "<leader>nn", require('conform').format, { desc = "Format buffer" })
   end,
 }
