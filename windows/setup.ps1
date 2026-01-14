ed3e099d0080741ca89a00887937123a8f6ca5bf
EMEA\POF1AV <francisco.power@pt.bosch.com>
2026-01-14 10:30:25 +0000

    update windows setup script to setup opencode

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -9,8 +9,12 @@ Install-Module PSReadLine -Force
 choco install fzf
 choco install neovim
 choco install zoxide
-choco install starship
+choco install oh-my-posh
+choco install opencode
+
+# choco install starship
 
 rm $PROFILE
 New-Item -Path $PROFILE -ItemType SymbolicLink -Value .\windows\Microsoft.PowerShell_profile.ps1
 New-Item -Path ~\AppData\Local\nvim -ItemType SymbolicLink -Value .\nvim\
+New-Item -Path ~\.config\opencode\opencode.jsonc -ItemType SymbolicLink -Value .\opencode.jsonc
