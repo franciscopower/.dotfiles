7468600c374500b9fa3cd49474e9b088ef0f79cc
EMEA\POF1AV <francisco.power@pt.bosch.com>
2026-02-04 22:44:28 +0000

    update debian setup

--- linux/setup_debian.sh
+++ linux/setup_debian.sh
@@ -25,13 +25,21 @@ echo -e "\n -----------------------------------------------"
 
 echo -e "\n Installing Dependecies..."
 # install dependencies
-yes | apt install python3
-yes | apt install python3-pip
-yes | apt install ranger
-yes | apt install neovim
-yes | apt install htop 
-yes | apt install neofetch  
+yes | apt install btop
+yes | apt install neofetch
 yes | apt install zsh 
+yes | apt install lazydocker
+
+# lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+
+# neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim-linux-x86_64
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 
 echo -e "\n -----------------------------------------------"
 
@@ -45,10 +53,10 @@ mv /root/.bashrc /root/.bashrc.bk
 ln -s $PWD/zsh/zshrc $folder/.zshrc
 ln -s $PWD/bash/bashrc $folder/.bashrc
 ln -s $PWD/../vimrc $folder/.vimrc
-ln -s $PWD/gitconfig $folder/.gitconfig
+ln -s $PWD/../gitconfig $folder/.gitconfig
 ln -s $PWD/../nvim $folder/.config/nvim
 # add settings to superuser too
-ln -s $PWD /root/.dotfiles
+ln -s $PWD/root/.dotfiles
 ln -s $PWD/zsh/zshrc /root/.zshrc
 ln -s $PWD/bash/bashrc /root/.bashrc
 ln -s $PWD/../vimrc /root/.vimrc
